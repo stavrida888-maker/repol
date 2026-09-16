720d896c7642e18d0c641efc7c8ca480d9e4c735
stavrida888-maker <stavrida888@gmail.com>
2026-09-16 09:32:49 +0500

    Update задание4.cpp

--- задание1/задание4.cpp
+++ задание1/задание4.cpp
@@ -1,13 +1,17 @@
-#include <iostream>
-#include <cmath>
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <math.h>
 
-int main() {
-    double a = 0.5;
-    double b = 0.5;
-    double y = sqrt((a * pow(sin(b), 2) + cos(pow(b, 3)) + cbrt(pow(b, 2))) / pow(fabs((a * tan(b)) / (1 - exp(sqrt(a)))), 0.25));
+int main() 
+{
+    double a, b, y;
 
-    std::cout << "y = " << y << std::endl;
+    printf("Vvedite a i b: ");
+    scanf("%lf %lf", &a, &b);
+
+    y = sqrt((pow(a, pow(sin(b), 2) + cos(pow(b, 3))) + cbrt(pow(b, 2))) / pow(fabs((a * tan(b)) / (1 - exp(sqrt(a)))), 0.25));
+
+    printf("y = %.3f\n", y);
 
     return 0;
 }
-неправильно!!!
