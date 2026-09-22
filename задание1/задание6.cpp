0aa96434a10b5115367f168bfc50c3fff70d5eba
stavrida888-maker <stavrida888@gmail.com>
2026-09-22 10:59:04 +0500

    Update задание6.cpp

--- задание1/задание6.cpp
+++ задание1/задание6.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
 int main()
 {
     int rub, kop;
@@ -5,26 +7,30 @@ int main()
     printf("Vvedite rubli i kopejki: ");
     scanf("%d %d", &rub, &kop);
 
-    int money = rub * 100 + kop;
+    int start_money = rub * 100 + kop;
+    int money = start_money;
     int max_money = money;
     int best_passes = 0;
     int passes = 0;
 
-    while (money >= 29 && passes < 1000)
+    if (money >= 29)
     {
-        passes += 1;
+        do{
+       
+            passes++;
 
-        money = money - 29;
+            money = money - 29;
 
-        int r = money / 100;
-        int k = money % 100;
+            int r = money / 100;
+            int k = money % 100;
 
-        money = k * 100 + r;
-        if (money > max_money)
+            money = k * 100 + r;
+            if (money > max_money)
         {
             max_money = money;
             best_passes = passes;
         }
+        } while (money >= 29 && money != start_money);
     }
 
     printf("max summa: %d r. %d k.\n", max_money / 100, max_money % 100);
@@ -32,4 +38,3 @@ int main()
 
     return 0;
 }
-нужно использовать do while
